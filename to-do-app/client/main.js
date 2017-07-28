d936056f701671ea49ca767cc7f7fbbbd21a7558
Lana-sloth <lana.sloth@gmail.com>
2017-07-28 15:17:57 +0300

    styling the pie chart

--- to-do-app/client/main.js
+++ to-do-app/client/main.js
@@ -21,3 +21,51 @@ Template.projectList.events({
 Template.projectList.rendered = function() {
     AnimatedEach.attachHooks(this.find(".list-group"));
 };
+
+//chart in projectList template
+Template.projectList.helpers({
+  topGenresChart: function() {
+    return {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+
+            backgroundColor: null,
+
+            spacingBottom: 0,
+            spacingTop: -15,
+            spacingLeft: 0,
+            spacingRight: 0,
+            width: 80,
+            height: 80
+        },
+        title: {
+            text: ""
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: false,
+                cursor: '',
+                dataLabels: {
+                    enabled: false,
+                    format: '{point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        colors: ['#337ab7','#ffffff'],
+        series: [{
+            type: 'pie',
+            name: 'tasks',
+            data: [
+                ['Done',   70],
+                ['Undone', 30]
+            ]
+        }]
+    };
+  }
+})
